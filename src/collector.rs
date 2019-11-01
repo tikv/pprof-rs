a2ab07a173e53550fcefc5ace676788f78510ad0
Yang Keao <keao.yang@yahoo.com>
2019-11-01 19:19:27 +0800

    use less stack space
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

--- src/collector.rs
+++ src/collector.rs
@@ -5,7 +5,7 @@ use std::hash::{Hash, Hasher};
 use std::io::{Read, Seek, SeekFrom, Write};
 use std::marker::PhantomData;
 
-pub const BUCKETS: usize = (1 << 16) / std::mem::size_of::<Entry<UnresolvedFrames>>();
+pub const BUCKETS: usize = (1 << 14) / std::mem::size_of::<Entry<UnresolvedFrames>>();
 pub const BUCKETS_ASSOCIATIVITY: usize = 4;
 pub const BUFFER_LENGTH: usize = (1 << 18) / std::mem::size_of::<Entry<UnresolvedFrames>>();
 
