8bed9a5480ee5749ac09bbd90bd85d9b3c0e59cc
YangKeao <keao.yang@yahoo.com>
2021-10-28 19:46:43 +0800

    fix memory leak in collector (#84)
    * fix memory leak in collector
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * use default for collector
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * allow uninit assumed init
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * use vector to initialize array
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- src/frames.rs
+++ src/frames.rs
@@ -26,6 +26,20 @@ pub struct UnresolvedFrames {
     pub thread_id: u64,
 }
 
+impl Default for UnresolvedFrames {
+    #[allow(clippy::uninit_assumed_init)]
+    fn default() -> Self {
+        let frames = unsafe { std::mem::MaybeUninit::uninit().assume_init() };
+        Self {
+            frames,
+            depth: 0,
+            thread_name: [0; MAX_THREAD_NAME],
+            thread_name_length: 0,
+            thread_id: 0,
+        }
+    }
+}
+
 impl Clone for UnresolvedFrames {
     fn clone(&self) -> Self {
         let slice = self.slice().clone();
