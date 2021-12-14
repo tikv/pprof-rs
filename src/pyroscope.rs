2b2d873dd7cd2f893c04b560d71230c28a703f0f
YangKeao <yangkeao@chunibyo.icu>
2021-12-14 11:25:58 +0800

    Squashed commit of the following:
    commit 764fd780811706ef2fcbfa0eb700aa33a754beb9
    Author: matthew.cobbing <cobbinma@gmail.com>
    Date:   Thu Nov 11 15:30:52 2021 +0000
    
        remove unnecessary scope
    
    commit 1ad68df9acb5ec4d19904700649f8c79642fd490
    Author: matthew.cobbing <cobbinma@gmail.com>
    Date:   Thu Nov 11 14:42:34 2021 +0000
    
        merge tags with application name
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- src/pyroscope.rs
+++ src/pyroscope.rs
@@ -2,6 +2,8 @@
 
 //! this mod could help you to upload profiler data to the pyroscope
 
+use std::collections::HashMap;
+
 use crate::Result;
 
 use tokio::sync::mpsc;
@@ -17,7 +19,9 @@ impl PyroscopeAgent {
         url: String,
         frequency: libc::c_int,
         application_name: String,
+        tags: Option<HashMap<String, String>>,
     ) -> Self {
+        let application_name = merge_tags_with_app_name(application_name, tags);
         let (stopper, mut stop_signal) = mpsc::channel::<()>(1);
 
         // Since Pyroscope only allow 10s intervals, it might not be necessary
@@ -51,5 +55,53 @@ impl PyroscopeAgent {
         self.handler.await.unwrap()?;
 
         Ok(())
+    }
+}
+
+fn merge_tags_with_app_name(
+    application_name: String,
+    tags: Option<HashMap<String, String>>,
+) -> String {
+    format!(
+        "{}{}",
+        application_name,
+        tags.map(|tags| tags
+            .into_iter()
+            .filter(|(k, _)| k != "__name__")
+            .map(|(k, v)| format!("{}={}", k, v))
+            .collect::<Vec<String>>())
+            .map(|mut tags| {
+                tags.sort();
+
+                format!("{{{}}}", tags.join(","))
+            })
+            .unwrap_or("".to_string())
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::pyroscope::merge_tags_with_app_name;
+
+    #[test]
+    fn merge_tags_with_app_name_with_tags() {
+        let mut tags = HashMap::new();
+        tags.insert("env".to_string(), "staging".to_string());
+        tags.insert("region".to_string(), "us-west-1".to_string());
+        tags.insert("__name__".to_string(), "reserved".to_string());
+        assert_eq!(
+            merge_tags_with_app_name("my.awesome.app.cpu".to_string(), Some(tags)),
+            "my.awesome.app.cpu{env=staging,region=us-west-1}".to_string()
+        )
+    }
+
+    #[test]
+    fn merge_tags_with_app_name_without_tags() {
+        assert_eq!(
+            merge_tags_with_app_name("my.awesome.app.cpu".to_string(), None),
+            "my.awesome.app.cpu".to_string()
+        )
     }
 }
