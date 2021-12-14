7771d5e49fa21fbc322b2d38105e9e7a8e7dd450
Alin Sinpalean <alin.sinpalean@gmail.com>
2021-12-14 13:38:47 +0100

    Add thread names and timing information to protobuf reports (#90)
    * Add thread names and timing information to protobuf reports
    
    Record thread names (or IDs); record "cpu: nanoseconds" in addition to
    "samples: count" value types; and the timestamp and duration of the collected
    profile; to protobuf reports.
    
    This matches exactly the format of profiles produced by Golang's
    `runtime/pprof` package. And, when inspected with `go tool pprof` default to
    displaying seconds spent in a specific location, as opposed to samples (whose
    counts depend on sampling frequency, requiring a lot of mental math to map to
    actual time). It is of course still possible to view samples instead of
    seconds, if wanted.
    
    Signed-off-by: Alin Sinpalean <alin.sinpalean@gmail.com>
    
    * Remove unnecessary default() call.
    
    Signed-off-by: Alin Sinpalean <alin.sinpalean@gmail.com>
    
    * Add thread names and timing information to protobuf reports
    
    Record thread names (or IDs); record "cpu: nanoseconds" in addition to
    "samples: count" value types; and the timestamp and duration of the collected
    profile; to protobuf reports.
    
    This matches exactly the format of profiles produced by Golang's
    `runtime/pprof` package. And, when inspected with `go tool pprof` default to
    displaying seconds spent in a specific location, as opposed to samples (whose
    counts depend on sampling frequency, requiring a lot of mental math to map to
    actual time). It is of course still possible to view samples instead of
    seconds, if wanted.
    
    Signed-off-by: Alin Sinpalean <alin.sinpalean@gmail.com>
    
    * Remove unnecessary default() call.
    
    Signed-off-by: Alin Sinpalean <alin.sinpalean@gmail.com>
    
    * Move import of SystemTime under mod protobuf.
    
    Signed-off-by: Alin Sinpalean <alin.sinpalean@gmail.com>

--- src/timer.rs
+++ src/timer.rs
@@ -2,6 +2,7 @@
 
 use std::os::raw::c_int;
 use std::ptr::null_mut;
+use std::time::{Duration, Instant, SystemTime};
 
 #[repr(C)]
 #[derive(Clone)]
@@ -24,7 +25,9 @@ extern "C" {
 const ITIMER_PROF: c_int = 2;
 
 pub struct Timer {
-    _frequency: c_int,
+    pub frequency: c_int,
+    pub start_time: SystemTime,
+    pub start_instant: Instant,
 }
 
 impl Timer {
@@ -48,7 +51,19 @@ impl Timer {
         };
 
         Timer {
-            _frequency: frequency,
+            frequency,
+            start_time: SystemTime::now(),
+            start_instant: Instant::now(),
+        }
+    }
+
+    /// Returns a `ReportTiming` struct having this timer's frequency and start
+    /// time; and the time elapsed since its creation as duration.
+    pub fn timing(&self) -> ReportTiming {
+        ReportTiming {
+            frequency: self.frequency,
+            start_time: self.start_time,
+            duration: self.start_instant.elapsed(),
         }
     }
 }
@@ -70,5 +85,26 @@ impl Drop for Timer {
                 null_mut(),
             )
         };
+    }
+}
+
+/// Timing metadata for a collected report.
+#[derive(Clone)]
+pub struct ReportTiming {
+    /// Frequency at which samples were collected.
+    pub frequency: i32,
+    /// Collection start time.
+    pub start_time: SystemTime,
+    /// Collection duration.
+    pub duration: Duration,
+}
+
+impl Default for ReportTiming {
+    fn default() -> Self {
+        Self {
+            frequency: 1,
+            start_time: SystemTime::UNIX_EPOCH,
+            duration: Default::default(),
+        }
     }
 }
