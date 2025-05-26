60606febd660ffe470414bb620d73b7a3d3b160b
Marco Concetto Rudilosso <marcoconcettorudilosso@gmail.com>
2025-05-26 13:55:30 +0200

    Add support for framehop as unwinder (#267)
    * Add framehow as an unwinder and support perfmaps
    
    perfmaps are reloaded on demand in a bg thread
    
    Signed-off-by: Marco Concetto Rudilosso <marco.rudilosso@shopify.com>
    
    * fix cargo fmt
    
    Signed-off-by: Marco Concetto Rudilosso <marco.rudilosso@shopify.com>
    
    ---------
    
    Signed-off-by: Marco Concetto Rudilosso <marco.rudilosso@shopify.com>
    Signed-off-by: Yang Keao <yangkeao@chunibyo.icu>
    Co-authored-by: Yang Keao <yangkeao@chunibyo.icu>

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,13 @@
 //! [README.md](https://github.com/tikv/pprof-rs/blob/master/README.md)
 
 /// Define the MAX supported stack depth. TODO: make this variable mutable.
+#[cfg(feature = "large-depth")]
+pub const MAX_DEPTH: usize = 1024;
+
+#[cfg(all(feature = "huge-depth", not(feature = "large-depth")))]
+pub const MAX_DEPTH: usize = 512;
+
+#[cfg(not(any(feature = "large-depth", feature = "huge-depth")))]
 pub const MAX_DEPTH: usize = 128;
 
 /// Define the MAX supported thread name length. TODO: make this variable mutable.
@@ -51,6 +58,8 @@ mod backtrace;
 mod collector;
 mod error;
 mod frames;
+#[cfg(feature = "perfmaps")]
+mod perfmap;
 mod profiler;
 mod report;
 mod timer;
